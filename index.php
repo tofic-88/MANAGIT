9336ca31bf1adcf76d6f47f2b88207428db80956
Tofic <mehrazitawfik@gmail.com>
2020-01-18 00:53:12 -0500

    just for test

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <div class="jumbotron jumbotron-fluid" id="header">
         <div class="container">
             <img id="logo" src="images/nw-logo.png" alt="NumidiaLogo">
-            <h1>MANAGIT</h1>
+            <h1>MANAGITo</h1>
             <h3>iF YOU DONT PROGRAM YOUR LIFE, LIFE DO IT</h3>
             <?php require 'menu.php'; ?>
         </div>
